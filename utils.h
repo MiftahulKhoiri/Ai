1c88f15def1ebfc125d61423fde22280f878a8cd
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2026-07-09 20:39:44 +0100

    Delete utils.h

--- utils.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "value.h"
-#include <vector>
-
-// Softmax: mengembalikan vektor probabilitas sebagai ValuePtr
-std::vector<ValuePtr> softmax(const std::vector<ValuePtr>& logits);
