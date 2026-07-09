fd14bfccbdb23221d0f1122651908e787aaf0152
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2026-07-09 15:36:06 +0100

    Update generate_data.py

--- generate_data.py
+++ generate_data.py
@@ -30,7 +30,7 @@ def generate_synthetic_data(num_sentences):
     return sentences
 
 if __name__ == "__main__":
-    num = 10
+    num = 20
     output = "data.json"
     if len(sys.argv) >= 2:
         num = int(sys.argv[1])
