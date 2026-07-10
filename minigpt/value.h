f03f84926c6b7034dc65d116f43c50c5f6c14dc7
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2026-07-10 06:30:01 +0100

    Update value.h

--- minigpt/value.h
+++ minigpt/value.h
@@ -1,37 +1,50 @@
 #pragma once
-#include <memory>
-#include <vector>
+
 #include <functional>
+#include <memory>
 #include <string>
+#include <vector>
 
 struct Value : std::enable_shared_from_this<Value> {
     double data;
     double grad;
+
     std::function<void()> _backward;
     std::vector<std::shared_ptr<Value>> _prev;
     std::string _op;
 
-    Value(double data, std::vector<std::shared_ptr<Value>> children = {}, std::string op = "");
+    explicit Value(
+        double data,
+        std::vector<std::shared_ptr<Value>> children = {},
+        std::string op = ""
+    );
+
     static std::shared_ptr<Value> create(double data);
+
     void backward();
     std::string repr() const;
 };
 
 using ValuePtr = std::shared_ptr<Value>;
 
-// Non-member operators
+// Arithmetic operators
 ValuePtr operator+(const ValuePtr& a, const ValuePtr& b);
 ValuePtr operator+(const ValuePtr& a, double b);
 ValuePtr operator+(double a, const ValuePtr& b);
-ValuePtr operator*(const ValuePtr& a, const ValuePtr& b);
-ValuePtr operator*(const ValuePtr& a, double b);
-ValuePtr operator*(double a, const ValuePtr& b);
+
 ValuePtr operator-(const ValuePtr& a, const ValuePtr& b);
 ValuePtr operator-(const ValuePtr& a, double b);
 ValuePtr operator-(double a, const ValuePtr& b);
+
+ValuePtr operator*(const ValuePtr& a, const ValuePtr& b);
+ValuePtr operator*(const ValuePtr& a, double b);
+ValuePtr operator*(double a, const ValuePtr& b);
+
 ValuePtr operator/(const ValuePtr& a, const ValuePtr& b);
 ValuePtr operator/(const ValuePtr& a, double b);
 ValuePtr operator/(double a, const ValuePtr& b);
+
+// Math functions
 ValuePtr pow(const ValuePtr& a, double exponent);
 ValuePtr exp(const ValuePtr& a);
 ValuePtr log(const ValuePtr& a);
