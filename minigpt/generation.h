9e7495f5772437135ad91106d2fff35db3bae548
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2026-07-11 14:47:01 +0100

    Update generation.h

--- minigpt/generation.h
+++ minigpt/generation.h
@@ -5,10 +5,14 @@
 #include <string>
 #include <vector>
 
-void generate(MiniGPT& model, ByteLevelBPETokenizer& tokenizer,
-              const std::string& prompt, int max_tokens = 50, 
-              bool add_bos = false, bool add_eos = false);
-
 // Fungsi pembantu
 int argmax(const std::vector<Value::Ptr>& logits);
-std::vector<int> sample_top_k(const std::vector<Value::Ptr>& logits, int k = 0, float temperature = 1.0);
+std::vector<int> sample_top_k(const std::vector<Value::Ptr>& logits, int k = 0, float temperature = 1.0f);
+
+// Fungsi generate utama
+void generate(MiniGPT& model, 
+              ByteLevelBPETokenizer& tokenizer,
+              const std::string& prompt, 
+              int max_tokens = 50, 
+              bool add_bos = false, 
+              bool add_eos = false);
