426ef6bec25d28426578c74745c3ec121a7854a3
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2026-07-11 14:30:28 +0100

    Update backward.h

--- minigpt/backward.h
+++ minigpt/backward.h
@@ -1,6 +1,12 @@
 // backward.h
 #pragma once
 #include "value.h"
+#include <memory>
+#include <cmath>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
 namespace autograd {
 namespace backward {
@@ -11,6 +17,7 @@ void mul(Value* out, Value::Ptr a, Value::Ptr b);
 void div(Value* out, Value::Ptr a, Value::Ptr b);
 void tanh(Value* out, Value::Ptr a);
 void relu(Value* out, Value::Ptr a);
+void gelu(Value* out, Value::Ptr a);
 
 } // namespace backward
 } // namespace autograd
